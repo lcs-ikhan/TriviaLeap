56c1d1eb6d929a575e8d00e00004a6080fc600b2
Isaad Khan <IKhan@lcs.on.ca>
2023-06-01 15:00:30 -0400

    Initial Commit

---
+++ TriviaLeap/TriviaLeapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaLeapApp.swift
+//  TriviaLeap
+//
+//  Created by Isaad Khan on 2023-06-01.
+//
+
+import SwiftUI
+
+@main
+struct TriviaLeapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
