298228979561af1da02031d7ef92d542f82080e7
Isaad Khan <IKhan@lcs.on.ca>
2023-06-09 09:02:24 -0400

    Started to add view to see the list of saved trivia

---
+++ TriviaLeap/Views/Second Views/SavedView.swift
@@ -0,0 +1,20 @@
+//
+//  SavedView.swift
+//  TriviaLeap
+//
+//  Created by Isaad Khan on 2023-06-09.
+//
+
+import SwiftUI
+
+struct SavedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SavedView_Previews: PreviewProvider {
+    static var previews: some View {
+        SavedView()
+    }
+}
